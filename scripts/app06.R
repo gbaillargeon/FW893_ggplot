55c1e1d0fad49dff3fe481377c5523e08c44b946
gbaillargeon <baillagr@msu.edu>
2020-07-17 15:47:43 -0400

    lesson 6

---
+++ scripts/app06.R
@@ -0,0 +1,83 @@
+# Create a vector that holds the quantile values for the 20, 40, 60, and 80th percentile of stnPressure.
+# Create a new column called pressureLevel that creates five evenly spaced levels for stnPressure
+# Make a boxplot of windSusSpeed vs pressureLevel
+# Find the dates for the three outliers at the lowest pressure level.  The three outliers are also the three highest windSusSpeed for the year.
+# Label the outliers for the lowest pressure level with the dates on the plot.
+
+#Part 1: Create vector with quantile values for stnPressure
+source( file="scripts/reference.R" ); 
+weatherData = read.csv( file="data/LansingNOAA2016-2.csv", 
+                        stringsAsFactors = FALSE );
+
+#quantile finds percentile values
+#probs gives the quantile values we want
+stnPressureQuant = quantile(weatherData$stnPressure, 
+                            probs=c(.20, .40, .60, .80));
+
+
+#Part 2: iterate through pressureLevel to assign 5 even levels 
+for(day in 1:nrow(weatherData)) # nrow(weatherData) = 366
+{
+  if(weatherData$stnPressure[day] <= stnPressureQuant[1]) 
+  {
+    weatherData$pressureLevel[day] = "Very Low";
+  }
+
+  else if(weatherData$stnPressure[day] > stnPressureQuant[1] && 
+          weatherData$stnPressure[day] <= stnPressureQuant[2]) 
+  {
+    weatherData$pressureLevel[day] = "LOw";
+  }
+  else if(weatherData$stnPressure[day] > stnPressureQuant[2] &&
+          weatherData$stnPressure[day] <= stnPressureQuant[3])
+  {
+    weatherData$pressureLevel[day] = "Medium";
+  }
+ else if(weatherData$stnPressure[day] > stnPressureQuant[3] &&
+         weatherData$stnPressure[day] <= stnPressureQuant[4])
+ {
+   weatherData$pressureLevel[day] = "Medium-High";
+ }
+  
+ else
+  {
+    weatherData$pressureLevel[day] = "High";
+  }
+}
+#save new dataframe
+write.csv(weatherData, file="data/LansingNOAA2016-6.csv");
+
+#Part 3-5
+#ordering pressure from low to high
+pressureFact = factor(weatherData$pressureLevel,
+                     levels=c("Very Low", "Low", "Medium", "Medium-High", "High"))
+
+#find outliers - 3 highest windSusSpeed
+#put wind speeds in order of highest to lowestest speed
+highWindSusSpeed = order(weatherData$windSusSpeed, decreasing = TRUE);
+topSpeeds = weatherData$windSusSpeed[highWindSusSpeed[1:3]];
+topDates = weatherData$date[highWindSusSpeed[1:3]];
+
+#plotting
+thePlot = ggplot(data=weatherData) +
+  geom_boxplot(mapping=aes(x=pressureFact, y=windSusSpeed),
+               na.rm=TRUE,
+               outlier.shape = 20, 
+               outlier.color = "red",
+               outlier.alpha = 0.6, 
+               outlier.size = 3 ) +
+  annotate(geom="text", 
+           x=1,
+           y=-topSpeeds[1],
+           color="darkorange",
+           label=topDates[1] )+
+  annotate(geom="text", x=1.5, color = "blue", y=topSpeeds[2], label=topDates[2])+
+  annotate(geom="text", x=2, color="black",  y=topSpeeds[3], label=topDates[3])
+  theme_bw() +
+  labs(title = "Wind Speed v. Pressure Level",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Pressure Level",
+       y = "Wind Speed");
+plot(thePlot);
+
+
---
+++ scripts/lesson06.R
@@ -0,0 +1,180 @@
+source( file="scripts/reference.R" ); 
+weatherData = read.csv( file="data/LansingNOAA2016-2.csv", 
+                        stringsAsFactors = FALSE );
+
+#quantile finds percentile values
+#probs gives the quantile values we want
+windSpeedQuant = quantile(weatherData$windSpeed, probs=c(.30, .70));
+
+#iterate through weatherData to assign high, med, low wind speed
+for(day in 1:nrow(weatherData)) # nrow(weatherData) = 366
+{
+  # if the value in windSpeed is less than or equal to the low quant value
+  if(weatherData$windSpeed[day] <= windSpeedQuant[1]) # <= 6.4
+  {
+    weatherData$windSpeedLevel[day] = "Low";
+  }
+  # if the value in windSpeed is greater than or equal to the high quant value
+  else if(weatherData$windSpeed[day] >= windSpeedQuant[2]) # >= 10.2
+  {
+    weatherData$windSpeedLevel[day] = "High";
+  }
+  else # the value in windSpeed is between 6.2 and 10.4
+  {
+    weatherData$windSpeedLevel[day] = "Medium";
+  }
+}  
+
+#assign wind directions
+for(day in 1:nrow(weatherData))
+{
+  # if the direction is greater than 315 OR less than 45 degrees
+  if(weatherData$windSusDir[day] >= 315 || 
+     weatherData$windSusDir[day] < 45)
+  {
+    weatherData$windDir[day] = "North";
+  }
+  # if the direction is greater than 45 AND less than 135 degrees
+  else if(weatherData$windSusDir[day] >= 45 && 
+          weatherData$windSusDir[day] < 135)
+  {
+    weatherData$windDir[day] = "East";
+  }
+  # if the direction is greater than 135 AND less than 225 degrees
+  else if(weatherData$windSusDir[day] >= 135 && 
+          weatherData$windSusDir[day] < 225)
+  {
+    weatherData$windDir[day] = "South";
+  }
+  else # the direction is between 225 and 315 degrees
+  {
+    weatherData$windDir[day] = "West";
+  }
+}
+
+#assigns temperature change data
+for(day in 1:nrow(weatherData))
+{
+  if( day == 1)  # no day before the first day
+  {
+    weatherData$changeMaxTemp[day] = NA; 
+  }
+  else  # subtract previous day's maxTemp from current day's maxTemp
+  {
+    weatherData$changeMaxTemp[day] = weatherData$maxTemp[day] -
+      weatherData$maxTemp[day-1];
+  }
+}
+#save new dataframe
+write.csv(weatherData, file="data/LansingNOAA2016-3.csv");
+
+#### Part 2: Plot Wind Speed vs. wind direction
+thePlot = ggplot(data=weatherData) +
+  geom_boxplot(mapping=aes(x=windDir, y=changeMaxTemp)) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+#### Part 3: Violin plot of Wind Speed vs. wind direction
+thePlot = ggplot(data=weatherData) +
+  geom_violin(mapping=aes(x=windDir, y=changeMaxTemp),
+              na.rm=TRUE) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+#### Part 4: Add error bars
+thePlot = ggplot(data=weatherData) +
+  stat_boxplot(mapping=aes(x=windDir, y=changeMaxTemp), 
+               na.rm=TRUE,
+               geom = "errorbar",
+               width = 0.2) +
+  geom_boxplot(mapping=aes(x=windDir, y=changeMaxTemp),
+               na.rm=TRUE) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+### Part 5: Re-order the directions on the x-axis using factor(s)
+windDirFact = factor(weatherData$windDir,
+                     levels=c("North", "East", "South", "West"));
+
+thePlot = ggplot(data=weatherData) +
+  stat_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE,
+               geom = "errorbar",
+               width = 0.2) +
+  geom_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+#changing outlier parameters
+thePlot = ggplot(data=weatherData) +
+  stat_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE,
+               geom = "errorbar",
+               width = 0.2) +
+  geom_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE,
+               outlier.shape = "@", 
+               outlier.color = "red",
+               outlier.alpha = 0.6, 
+               outlier.size = 4 ) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+#adding text to plot
+#finding rows that meet a condition
+northVals=which(weatherData$windDir == "North");
+southVals=which(weatherData$windDir == "South");
+northMed = median(weatherData[northVals,"changeMaxTemp"], na.rm=TRUE);
+southMed = median(weatherData[southVals,"changeMaxTemp"], na.rm=TRUE);
+
+#annotate plot with median values
+thePlot = ggplot(data=weatherData) +
+  stat_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE,
+               geom = "errorbar",
+               width = 0.2) +
+  geom_boxplot(mapping=aes(x=windDirFact, y=changeMaxTemp),
+               na.rm=TRUE,
+               outlier.shape = "@",
+               outlier.color = "red",
+               outlier.alpha = 0.6,
+               outlier.size = 4 ) +
+  annotate(geom="text", # North median
+           x=1,
+           y=20, 
+           color="blue",
+           label=paste("median:", northMed) ) +
+  annotate(geom="text", # South median
+           x=3, 
+           y=-10, 
+           color="red", 
+           label=paste("median:", southMed) ) +
+  theme_bw() +
+  labs(title = "Change in Temperature vs. Wind Direction",
+       subtitle = "Lansing, Michigan: 2016",
+       x = "Wind Direction",
+       y = "Degrees (Fahrenheit)");
+plot(thePlot);
+
+
