26910077f9eab8fe88b67fca9a1b8f5f99aa939d
gbaillargeon <baillagr@msu.edu>
2020-08-12 13:23:55 -0400

    finishing up

--- scripts/app09.R
+++ scripts/app09.R
@@ -1,6 +1,7 @@
 source( file="scripts/reference.R" ); 
 weatherData = read.csv( file="data/LansingNOAA2016-3.csv", 
                         stringsAsFactors = FALSE );
+
 #Need to convert Precip to numerics excluding 'T'
 
 for(i in 1:nrow(weatherData))
@@ -17,30 +18,12 @@ for(i in 1:nrow(weatherData))
 
 weatherData$precipVal = as.numeric(weatherData$precipVal)
 avgTemp=weatherData$avgTemp
-medTemps=c()
-hotTemps=c()
-coldTemps=c()
-for(i in 1:nrow(weatherData))
-{
-  if(avgTemp <= 55)
-  {
-    avgTemp=medTemps;
-  }
-  if(avgTemp > 55 && avgTemp<76)
-  { 
-     avgTemp=coldTemps;
-  }
-  else
-  {
-    avgTemp=hotTemps;
-  }
-}
 
 #Text Plot of Precip vs. Humidity
 thePlot = ggplot(data=weatherData) +
   geom_text(mapping=aes(x=precipVal, y=relHum, color=1:nrow(weatherData), label=avgTemp)) +
   scale_color_gradientn(colors = c("blue", "darkgreen", "red"),
-                        breaks=c(hotTemps, medTemps, coldTemps),
+                        breaks=c(0.8, 0.65, 0.3),
                         labels=c("hot", "moderate", "cold"))+
   scale_y_continuous(trans="log10")+
   theme_bw() +
